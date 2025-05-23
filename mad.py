60632188d08e7e9df94f6c0cb343c6b5c9c20d67
Madhankumarr11 <srmadhan95@gmail.com>
2025-05-23 22:05:11 +0530

    I Love Chennai

---
+++ mad.py
@@ -0,0 +1,19 @@
+import psycopg2
+
+connection = psycopg2.connect(
+    host="localhost",
+    port = "5432"
+    user="postgres",
+    password="01234"
+)
+
+cursor = connection.cursor()
+
+
+cursor.execute("select version()")
+
+version = cursor.fetchone()
+
+print(version)
+
+connection.close()
